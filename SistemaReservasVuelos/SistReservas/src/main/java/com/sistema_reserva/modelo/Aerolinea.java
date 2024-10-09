264e0659960e6358bf47ceabf13a2362554f50d2
exe <exequielbrites8@gmail.com>
2024-10-09 14:56:43 -0300

    Documentacion Reserva de vuelo

--- SistemaReservasVuelos/SistReservas/src/main/java/com/sistema_reserva/modelo/Aerolinea.java
+++ SistemaReservasVuelos/SistReservas/src/main/java/com/sistema_reserva/modelo/Aerolinea.java
@@ -3,9 +3,9 @@ package com.sistema_reserva.modelo;
 import java.util.List;
 
 /**
- * Clase Aerolinea:
+ * Representa una clase  Aerolinea:
  * 
- * @author Exequiel
+ * 
  */
 public class Aerolinea {
 
@@ -21,12 +21,49 @@ public class Aerolinea {
      * @param nombre       nombre de la aerolinea
      * @param listaAviones listado de aviones
      * @param listaVuelos  listado de vuelos
-     * @return nueva instancia de la clase Aerolinea
+     * 
      */
     public Aerolinea(int idAaerolinea, String nombre, List<Avion> listaAviones, List<Vuelo> listaVuelos) {
         this.idAaerolinea = idAaerolinea;
         this.nombre = nombre;
         this.listaAviones = listaAviones;
         this.listaVuelos = listaVuelos;
+    }
+
+    /**
+     * Agrega un avion a la lista de aviones de la aerolinea
+     * 
+     * @param avion avion a agregar
+     */
+    public void agregarAvion(Avion avion) {
+    
+        this.listaAviones.add(avion);
+    }
+
+    /**
+     * Agrega un vuelo a la lista de vuelos de la aerolinea
+     * 
+     * @param vuelo vuelo a agregar
+     */
+    public void agregarVuelo(Vuelo vuelo) {
+        this.listaVuelos.add(vuelo);
+    }
+
+    /**
+     * Devuelve la lista de aviones de la aerolinea
+     * 
+     * @return lista de aviones
+     */
+    public List<Avion> obtenerAviones() {
+        return this.listaAviones;
+    }
+
+    /**
+     * Devuelve la lista de vuelos de la aerolinea
+     * 
+     * @return lista de vuelos
+     */
+    public List<Vuelo> obtenerVuelos() {
+        return this.listaVuelos;
     }
 }
--- SistemaReservasVuelos/SistReservas/src/main/java/com/sistema_reserva/modelo/Cliente.java
+++ SistemaReservasVuelos/SistReservas/src/main/java/com/sistema_reserva/modelo/Cliente.java
@@ -1,5 +1,44 @@
 package com.sistema_reserva.modelo;
 
+import java.util.List;
+
+/**
+ * Representa una clase cliente
+ */
 public class Cliente {
 
+    private int idCliente;
+    private String nombre;
+    private List<Reserva> listaReservas;
+
+    /**
+     * Crea un nuevo cliente
+     * 
+     * @param idCliente     identificador del cliente
+     * @param nombre        nombre del cliente
+     * @param listaReservas lista de reservas hechas por este cliente
+     */
+    public Cliente(int idCliente, String nombre, List<Reserva> listaReservas) {
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+        this.listaReservas = listaReservas;
+    }
+
+    /**
+     * Reserva un vuelo para este cliente.
+     * 
+     * @param reserva reserva a realizar
+     */
+    public void reservarVuelo(Reserva reserva) {
+        this.listaReservas.add(reserva);
+    }
+
+    /**
+     * Devuelve la lista de reservas hechas por este cliente.
+     * 
+     * @return lista de reservas
+     */
+    public List<Reserva> obtenerReservas() {
+        return this.listaReservas;
+    }
 }
